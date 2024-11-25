69f3e837dccb7fceced3c9e41b675504f651b290
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2024-11-25 11:57:13 -0500

    feat: :sparkles: feat: :sparkles: Add invariant = true

--- obj/Debug/net6.0/CS_AWS_EC2.AssemblyInfo.cs
+++ obj/Debug/net6.0/CS_AWS_EC2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS_AWS_EC2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3479b0068170013a39b2e3d824db5e620801ff1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS_AWS_EC2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS_AWS_EC2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
